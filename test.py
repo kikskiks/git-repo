c1c5bf5aba98bd5169507212baa10449cd6f02b8
kikskiks <alex.kiks.kikin@gmail.com>
2015-11-17 17:23:53 +0400

    test

--- test.py
+++ test.py
@@ -1,2 +1,22 @@
-__author__ = 'Alex'
-print('test')
+from tkinter import *
+root = Tk()
+root.title("PythonicWay Snake")
+
+# ширина экрана
+WIDTH = 800
+# высота экрана
+HEIGHT = 600
+# Размер сегмента змейки
+SEG_SIZE = 20
+# Переменная отвечающая за состояние игры
+IN_GAME = True
+# создаем экземпляр класса Canvas (его мы еще будем использовать) и заливаем все зеленым цветом
+c = Canvas(root, width=WIDTH, height=HEIGHT, bg="#003300")
+c.grid()
+# Наводим фокус на Canvas, чтобы мы могли ловить нажатия клавиш
+c.focus_set()
+
+
+
+
+root.mainloop()
